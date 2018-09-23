38e8e0015472cecf159b714b76d0c1cf516f4dc3
Zetrox-X <polinyakteam@gmail.com>
2018-09-23 20:22:03 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   let count = 0;
+    for(let i = 0; i < preferences.length; i++){
+        let a = preferences[i];
+        let b = preferences[a-1];
+        let c = preferences[b-1];
+        if (i+1 == c && c != b && c!= a) {
+            count++;
+            preferences[i]=0;
+            preferences[a-1]=0;
+            preferences[b-1]=0;
+        }
+    }
+    return count;
+ };
 };
